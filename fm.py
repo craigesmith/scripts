b87260a360d68da8452b852a90121540a6da11d7
Craig Smith <craigesmith@gmail.com>
2020-07-09 00:26:29 -0400

    Update fm.py

--- fm.py
+++ fm.py
@@ -2,24 +2,31 @@
 # craig smith - july 8, 2020
 #
 
-a = {'a': {'PublicKey': 'a/key=', 'AllowedIPs': '10.1.0.1/16', 'Endpoint': '1.1.1.1:7000'},
-     'b': {'PublicKey': 'b/key=', 'AllowedIPs': '10.2.0.1/16', 'Endpoint': '2.2.2.2:7000'},
-     'c': {'PublicKey': 'c/key=', 'AllowedIPs': '10.3.0.1/16', 'Endpoint': '3.3.3.3:7000'},
-     'd': {'PublicKey': 'e/key=', 'AllowedIPs': '10.4.0.1/16', 'Endpoint': '4.4.4.4:7000'}}
+p = {1: {'PublicKey': 'a/key=', 'AllowedIPs': '10.1.0.1/16', 'Endpoint': '1.1.1.1:7000'},
+     2: {'PublicKey': 'b/key=', 'AllowedIPs': '10.2.0.1/16', 'Endpoint': '2.2.2.2:7000'},
+     3: {'PublicKey': 'c/key=', 'AllowedIPs': '10.3.0.1/16', 'Endpoint': '3.3.3.3:7000'},
+     4: {'PublicKey': 'd/key=', 'AllowedIPs': '10.4.0.1/16', 'Endpoint': '4.4.4.4:7000'}}
 
 l = []
 
-for x in a.keys():
-    for y in a.keys():
+for x in p.keys():
+    for y in p.keys():
         if x == y:
             pass
         else:
-            if (x + y) and (y + x) not in l:
-                l.append(x + y)
+            if (x, y) and (y, x) not in l:
+                l.append((x, y))
 
-for e in a.keys():
-    print('Server: ', e)
-    for h in l:
-        if e in h:
-            print(a[(h.replace(e, ''))])
+print(l)
 
+for each in p.keys():
+    print('Server: ', each)
+    for e in l:
+        x, y = e
+        if x == each:
+            print(p[y])
+        elif y == each:
+            print(p[x])
+
+
+            #print(p[(y.replace(x, ''))])
