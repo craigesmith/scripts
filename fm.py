624244d82ad3c525ebba82adc1821134856ebf9e
Craig Smith <craigesmith@gmail.com>
2020-07-14 13:53:46 -0400

    Update fm.py

--- fm.py
+++ fm.py
@@ -2,15 +2,15 @@
 # craig smith - july 8, 2020
 #
 
-p = {1: {'PublicKey': 'a/key=', 'AllowedIPs': '10.1.0.1/16', 'Endpoint': '1.1.1.1:7000'},
-     2: {'PublicKey': 'b/key=', 'AllowedIPs': '10.2.0.1/16', 'Endpoint': '2.2.2.2:7000'},
-     3: {'PublicKey': 'c/key=', 'AllowedIPs': '10.3.0.1/16', 'Endpoint': '3.3.3.3:7000'},
-     4: {'PublicKey': 'd/key=', 'AllowedIPs': '10.4.0.1/16', 'Endpoint': '4.4.4.4:7000'}}
+peers = {1: {'PublicKey': 'a/key=', 'AllowedIPs': '10.1.0.1/16', 'Endpoint': '1.1.1.1:7000'},
+         2: {'PublicKey': 'b/key=', 'AllowedIPs': '10.2.0.1/16', 'Endpoint': '2.2.2.2:7000'},
+         3: {'PublicKey': 'c/key=', 'AllowedIPs': '10.3.0.1/16', 'Endpoint': '3.3.3.3:7000'},
+         4: {'PublicKey': 'd/key=', 'AllowedIPs': '10.4.0.1/16', 'Endpoint': '4.4.4.4:7000'}}
 
 l = []
 
-for x in p.keys():
-    for y in p.keys():
+for x in peers.keys():
+    for y in peers.keys():
         if x == y:
             pass
         else:
@@ -19,14 +19,11 @@ for x in p.keys():
 
 print(l)
 
-for each in p.keys():
+for each in peers.keys():
     print('Server: ', each)
     for e in l:
         x, y = e
         if x == each:
-            print(p[y])
+            print(peers[y])
         elif y == each:
-            print(p[x])
-
-
-            #print(p[(y.replace(x, ''))])
+            print(peers[x])
